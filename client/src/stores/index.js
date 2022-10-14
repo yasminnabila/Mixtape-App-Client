8a7509d0cbfc256439d6daf29b79ecefe2ccd2de
yasminnabila <yasmnnabila@gmail.com>
2022-10-14 14:09:17 +0700

    Change baseUrl to Heroku

--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-// const baseUrl = "https://iproject-mixtape.herokuapp.com";
-const baseUrl = "http://localhost:3000";
+const baseUrl = "https://iproject-mixtape.herokuapp.com";
+// const baseUrl = "http://localhost:3000";
 
 export const useIndexStore = defineStore("index", {
   state: () => {
